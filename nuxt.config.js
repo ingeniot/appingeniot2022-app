b3cc7a8f756bd5b50a4d01ca698a9755e1d0f61c
ingeniot <ingeniotar@gmail.com>
2022-02-26 02:37:11 -0300

    favicon 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,10 +19,9 @@ export default {
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css' },
       { rel: 'apple-touch-icon', sizes: "180x180", href="/apple-touch-icon.png"},
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'},
-      { rel: 'icon', type: 'image/png', sizes="16x16", href: '/favicon-16x16.png'},
+      { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/favicon-16x16.png'},
       { rel: 'manifest', href: '/site.webmanifest'},
       { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color='#5bbad5'}
-
     ],
     bodyAttrs: {
       class: '' // Add `white-content` class here to enable "white" mode.
